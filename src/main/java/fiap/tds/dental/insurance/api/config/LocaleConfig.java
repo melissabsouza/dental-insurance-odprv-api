ec1721a5a4c05749d1ea56bfb7eac98a0cb527a4
Melissa <melissabsouzaa@gmail.com>
2025-05-12 10:16:15 -0300

    feat: internacionalization now working

--- src/main/java/fiap/tds/dental/insurance/api/config/LocaleConfig.java
+++ src/main/java/fiap/tds/dental/insurance/api/config/LocaleConfig.java
@@ -1,9 +1,7 @@
 package fiap.tds.dental.insurance.api.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,7 +16,7 @@ public class LocaleConfig implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver resolver = new SessionLocaleResolver();
-        resolver.setDefaultLocale(new Locale("pt", "BR"));
+        resolver.setDefaultLocale(Locale.forLanguageTag("pt-BR"));
         return resolver;
     }
 
@@ -32,12 +30,5 @@ public class LocaleConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
-    }
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
     }
 }
